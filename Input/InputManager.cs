1745c926c903e08da01387ae66a069fbdd2ab8f1
aaroneva <aaronalvaradocalderon@gmail.com>
2025-01-05 19:45:05 -0600

    aading new stuff to input manager

--- Input/InputManager.cs
+++ Input/InputManager.cs
@@ -1,18 +1,19 @@
+using System.Linq.Expressions;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    private PlayerInput playerInput;
-    private InputAction lookAction;
-    private InputActionAsset inputActionAsset;
-    
+    protected PlayerInput playerInput;
+    protected InputAction lookAction;
+    protected InputActionAsset inputActionAsset;
+    protected PlayerInputManager playerInputManager;
 
 
     private void Awake() {
-        GetPlayerInput();
+        GetUnityPlayerInputManager();
     }
-    public void GetPlayerInput() {
+    protected void GetPlayerInput() {
         try {
         playerInput = GetComponent<PlayerInput>(); 
         } catch (System.Exception e) {
@@ -20,30 +21,36 @@ public class InputManager : MonoBehaviour
         }
     }
 
-    public Vector2 GetMovementInput() {
-        return playerInput.actions["Move"].ReadValue<Vector2>();    
+    protected void GetUnityPlayerInputManager() {
+        try {
+            GetComponent<PlayerInputManager>(); 
+        } catch (System.Exception e) {
+            Debug.LogError(e);
+        }
     }
 
-    public Vector2 GetCameraLookInput() {
+
+
+    protected Vector2 GetCameraLookInput() {
         return playerInput.actions["Look"].ReadValue<Vector2>();
+    }
+
+    protected Vector2 GetMovementInput() {
+        return playerInput.actions["Move"].ReadValue<Vector2>();
     }
 
     public bool GetSprintInput() {
         return playerInput.actions["Sprint"].IsInProgress();
     }
-    public bool GetJumpInput() {
+    protected bool GetJumpInput() {
         return playerInput.actions["Jump"].WasPressedThisFrame();
     }
 
-    public string GetInputType() {
-        return playerInput.currentControlScheme.ToString();
-    }
+    protected string GetInputType { get { return playerInput.currentControlScheme.ToString(); } }
 
-    public string GetCurrentActionMap() {
-        return playerInput.currentActionMap.name;
-    }
+    protected string GetCurrentActionMap { get { return playerInput.currentActionMap.name; } }
 
-    public void SwitchActionMap(string actionMap) {
+    protected void SwitchActionMap(string actionMap) {
         playerInput.SwitchCurrentActionMap(actionMap);
     }
 
