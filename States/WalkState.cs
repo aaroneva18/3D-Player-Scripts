383f3c777954c025e84ae95a385d37076aa44872
aaroneva <aaronalvaradocalderon@gmail.com>
2025-01-19 22:18:47 -0600

    updating walk state

--- States/WalkState.cs
+++ States/WalkState.cs
@@ -3,20 +3,26 @@ using UnityEngine;
 public class WalkState : IState
 {
     private PlayerMovement movement;
+    private InputManager inputManager;
+    private StateMachine stateMachine;
 
-    public WalkState(PlayerMovement p_movement) {
+    public WalkState(PlayerMovement p_movement, InputManager p_inputManager, StateMachine p_stateMachine) {
         movement = p_movement;
+        inputManager = p_inputManager;
+        stateMachine = p_stateMachine;
     }
 
     public void Enter() {
         Debug.Log("Walk state enter");
+        movement.SetPlayable(true);
+        movement.SetCurrentSpeed(7f);   
     }
 
-    public void Execute() {
+    public void Execute(){
     }
+
     public void FixedExecute() {
         movement.Move();
-        
     }
 
     public void Exit() {
