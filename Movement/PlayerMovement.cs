2807a007c3bef5252e27f1e8e4280a6da5e0619b
aaroneva <aaronalvaradocalderon@gmail.com>
2025-01-19 23:27:48 -0600

    adding GetIsPlayable condition to Move and Jump function

--- Movement/PlayerMovement.cs
+++ Movement/PlayerMovement.cs
@@ -8,30 +8,35 @@ public class PlayerMovement : Movement
         SetDefaultState();
     }
 
-    void Update(){}
-
-    private void FixedUpdate() {
-        if (GetIsPlayable) {
-            if (CheckIsGrounded()) {
-                Move(); 
-                MatchRotation();
-            }
-        }
+    private void Update() {
+        
     }
 
     public override void Move() {
-        Vector2 input = InputPlayerManager.GetMovementInput();
-        Vector3 movement = (input.y * CameraTransform.forward) + (input.x * CameraTransform.right); 
-        Vector3 targetPosition = rb.position + movement * CalculateCurrentSpeed() * Time.deltaTime;
-        rb.MovePosition(targetPosition);
+        if (GetIsPlayable) {
+            Vector2 input = InputPlayerManager.GetMovementInput();
+            Vector3 movement = (input.y * CameraTransform.forward) + (input.x * CameraTransform.right);
+            Vector3 targetPosition = rb.position + movement * CalculateCurrentSpeed() * Time.deltaTime;
+            if (CheckIsGrounded()) {
+                rb.MovePosition(targetPosition);
+            } 
+        }
+    }
+
+    public override void Jump() {
+        rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        if (CheckIsGrounded() || GetIsPlayable) { 
+            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+            
+        }
+    }
+    
+    public override bool CheckIsMoving() {
+        return InputPlayerManager.GetMovementInput() != Vector2.zero ? true : false;
     }
 
     public void MatchRotation() {
         transform.Rotate(transform.rotation.x, CameraTransform.rotation.y + transform.rotation.y, transform.rotation.z);
-    }
-
-    public override float CalculateCurrentSpeed() {
-        return currentSpeed = InputPlayerManager.GetSprintInput() ? runSpeed : walkSpeed;
     }
 
     public override void SetDefaultState() {
@@ -41,9 +46,15 @@ public class PlayerMovement : Movement
             if (CameraTransform == null) { GameObject.Find("Main Camera").GetComponent<Transform>(); }
             SetPlayable(true);
             walkSpeed = 5;
-            runSpeed = 7;
+            sprintSpeed = 7;
         }catch (System.Exception e) {
             Debug.LogError(e);
         }
     }
+
+    public override float CalculateCurrentSpeed() {
+        float speed = InputPlayerManager.GetSprintInput() ? sprintSpeed : walkSpeed;
+        return speed;
+    }
+
 }
