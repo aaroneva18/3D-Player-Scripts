72a909a3f3413334b80b64f95996367b616f1cd1
aaroneva <aaronalvaradocalderon@gmail.com>
2025-01-21 19:31:10 -0600

    updating Player Inventary, removing the RayCast responsability

--- Player Manager/PlayerInventary.cs
+++ Player Manager/PlayerInventary.cs
@@ -1,65 +1,46 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerInventary : MonoBehaviour
 {
-    private Dictionary<string, GameObject> inventary;
+    public SerializedDictionary<string, GameObject> Inventary;
+
     private InputManagerPlayer inputManagerPlayer;
 
-    private float RayLenght = 0.0f;
-    private Ray ray;
 
     private void Awake() {
         SetDefaultState();
     }
     
-    void Start()
-    {
-        
-    }
-
+    void Start() {}
     
-    void Update()
-    {
-        
+    void Update() {}
+
+    public int GetInventarySize { get { return Inventary.Count; } }   
+    public GameObject GetItem(string name) {
+        return Inventary[name];
     }
 
-    private void AddItemToDictionary(string name, GameObject item) {
-        inventary.Add(name, item);
-    }
-
-    public void CollectItemOnRaycastHit() {
-        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, RayLenght)) {
-            if (inputManagerPlayer.GetCollectInput() && hit.collider.CompareTag("Item")) {
-                AddItemToDictionary(hit.collider.name, hit.collider.gameObject);
-                hit.collider.gameObject.SetActive(false); //make the item disappear
-            }
-        }
+    public void AddItemToInventary(string name, GameObject item) {
+        Inventary.Add(name, item);
     }
 
     public void RemoveItem(string name) {
-        inventary.Remove(name);
+        Inventary.Remove(name);
     }
 
-    public GameObject GetItem(string name) {
-        return inventary[name];
+    public void ClearInventary() {
+        Inventary.Clear();
     }
 
     public bool HasItem(string name) {
-        return inventary.ContainsKey(name);
+        return Inventary.ContainsKey(name);
     }
 
     private void SetDefaultState() {
-        inventary = new Dictionary<string, GameObject>();
+        Inventary = new SerializedDictionary<string, GameObject>();
         inputManagerPlayer = GetComponent<InputManagerPlayer>();
-        RayLenght = 2.0f;
-    }
-
-    private void OnDrawGizmos() {
-        Gizmos.color = Color.green;
-        Gizmos.DrawRay(ray.origin, ray.direction * RayLenght);
     }
 
 }
