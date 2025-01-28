53ae4cc1145c648dabbf2e9f474e898a4758f756
aaroneva <aaronalvaradocalderon@gmail.com>
2025-01-27 21:32:33 -0600

    improving Inventary State

--- States/InventaryState.cs
+++ States/InventaryState.cs
@@ -7,25 +7,30 @@ public class InventaryState : IState
 {
     private PlayerInventary playerInventary;
     private PlayerMovement playerMovement;
+    private InputManagerPlayer inputManager;
 
-    public InventaryState(PlayerInventary p_playerInventary, PlayerMovement p_playerMovement) {
+    public InventaryState(PlayerInventary p_playerInventary, PlayerMovement p_playerMovement, InputManagerPlayer p_inputManager) {
         playerInventary = p_playerInventary;
         playerMovement = p_playerMovement;
+        inputManager = p_inputManager;
     }
 
     public void Enter() {
         playerMovement.SetPlayable(false);
         playerInventary.SetPanelActive(true);
+        inputManager.SwitchActionMap("Inventary");
     }
 
     public void Execute() {
 
     }
-
-    public void Exit() {
-        
-    }
-
     public void FixedExecute() {
     }
+
+    public void Exit() {
+        playerInventary.SetPanelActive(false);
+        playerMovement.SetPlayable(true);
+        inputManager.SwitchActionMap("Gameplay");
+    }
+
 }
