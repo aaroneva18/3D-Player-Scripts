6cecee2dd5e96fefa46f52ab650e89cc42d94b9a
aaroneva <aaronalvaradocalderon@gmail.com>
2025-01-27 22:01:53 -0600

    adding PutObjectOnRightHand function and making it work in InteractWithItem

--- Player Manager/PlayerInteractable.cs
+++ Player Manager/PlayerInteractable.cs
@@ -9,8 +9,10 @@ public class PlayerInteractable : MonoBehaviour
     private Ray ray;
 
     [SerializeField] private float RayLenght = 0.0f;
+    [SerializeField] private bool IsRigthHandEmpty = true;
     [SerializeField] private LayerMask interactableLayer;
     [SerializeField] private Transform CameraTransform;
+    [SerializeField] private Transform RigthHand;
 
     private void Awake() {
         SetDefaultState();
@@ -33,13 +35,22 @@ public class PlayerInteractable : MonoBehaviour
         if (Physics.Raycast(CameraTransform.position, CameraTransform.forward, out hit, RayLenght, interactableLayer)) {
             //Aqui deberia de mostar la UI
             if (inputManger.GetCollectInput() && inventary.GetInventarySize < inventary.GetInventoryMaxSize) {
-                Debug.Log("Inventary size: " + inventary.GetInventarySize);
                 inventary.AddItemToInventary(hit.collider.gameObject.name, hit.collider.gameObject);
-                hit.collider.gameObject.SetActive(false);
-            } else if(inputManger.GetCollectInput() && inventary.GetInventarySize >= inventary.GetInventoryMaxSize) {
+                if (IsRigthHandEmpty) {
+                   PutObjectOnRigthHand(hit.collider.gameObject);
+                } 
+            } 
+            else if(inputManger.GetCollectInput() && inventary.GetInventarySize >= inventary.GetInventoryMaxSize) {
                 Debug.Log("Inventary is full");
             }
         }
+    }
+
+    private void PutObjectOnRigthHand(GameObject p_object) {
+        p_object.GetComponent<Rigidbody>().isKinematic = true;
+        //Mejorar el como se pone la posicion del objeto en la mano y la rotacion
+        p_object.transform.position = RigthHand.position;
+        p_object.transform.SetParent(RigthHand);
     }
 
     private void SetDefaultState() {
