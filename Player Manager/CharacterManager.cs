13d819638f23a3191152c7c293b350376cf00120
aaroneva <aaronalvaradocalderon@gmail.com>
2025-01-27 21:12:46 -0600

    adding the Character Manager script

---
+++ Player Manager/CharacterManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharacterManager : MonoBehaviour
+{
+
+    public Transform InitialCharacterPosition;
+
+    [SerializeField] protected int health = 0;
+    [SerializeField] protected int maxHealth = 100;
+    [SerializeField] protected bool isAlive = true;
+    [SerializeField] protected bool isDead = false;
+
+    public int GetHealth { get { return health; } }
+    public int GetMaxHealth { get { return maxHealth; } }
+    public bool GetIsAlive { get { return isAlive; } }
+    public bool GetIsDead { get { return isDead; } }
+
+    public abstract void Dead();
+
+    public abstract void Respawn();
+
+    public abstract void SetDefaultState();
+
+    public void TakeDamage(int p_damage) {
+        if (p_damage >= maxHealth) { Dead(); }
+        health -= p_damage;
+        if (health <= 0) { Dead(); }
+    }
+
+    public void Heal(int p_heal) {
+        if (p_heal > maxHealth || (p_heal + health) > maxHealth) { health = maxHealth; }
+        health += p_heal;
+    }
+    public void TeleportTo(Transform desirePosition) {
+        transform.position = desirePosition.position;
+    }
+
+}
