571349e5e75dd8e6ece41ca6c0e5749e71ef2119
aaroneva <aaronalvaradocalderon@gmail.com>
2025-01-27 21:30:46 -0600

    improving PlayerManager by making it a CharacterManager's son

--- Player Manager/PlayerManager.cs
+++ Player Manager/PlayerManager.cs
@@ -1,18 +1,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerManager : MonoBehaviour {
+public class PlayerManager : CharacterManager {
 
     public PlayerInventary inventary;
-    public Transform InitialPlayerPosition;
     public List<Transform> checkPoints;
-
-    //Crear una lista de checkpoints para que el jugador pueda regresar a ellos
-
-    [SerializeField] private int health = 100;
-    [SerializeField] private int maxHealth = 100;
-    [SerializeField] private bool isAlive = true;
-    [SerializeField] private bool isDead = false;
 
     private PlayerMovement movement;
 
@@ -20,70 +12,52 @@ public class PlayerManager : MonoBehaviour {
         SetDefaultState();
     }
 
-    void Start()
-    {
-        //TeleportTo(GetLastCheckPoint());
+    public override void Spawn() {
+        TeleportTo(InitialCharacterPosition);
+        Heal(maxHealth);
+        isAlive = true;
+        isDead = false;
+        movement.SetPlayable(true);
     }
 
-    void Update()
-    {
-        
-    }
-
-    public int GetHealth { get { return health; } }
-    public int GetMaxHealth { get { return maxHealth; } }
-    public bool GetIsAlive { get { return isAlive; } }
-    public bool GetIsDead { get { return isDead; } }
-
-    public void TakeDamage(int p_damage) {
-        if (p_damage >= maxHealth) { Dead(); }
-        health -= p_damage;
-        if (health <= 0) { Dead(); }
-    }
-
-    public void Heal(int p_heal) {
-        if (p_heal > maxHealth || (p_heal + health) > maxHealth) { health = maxHealth; }
-        health += p_heal;
-    }
-
-    public void Dead() {
+    public override void Dead() {
         isAlive = false;
         isDead = true;
         movement.SetPlayable(false);
     }   
 
-    public void TeleportTo(Transform desiredPosition) {
-        if(desiredPosition == null){ 
-            Debug.Warning("Desire Position is null"); 
-            return;
-        }
-        transform.position = desirePosition.position;
+    public override void Respawn() {
+        TeleportTo(GetLastCheckPoint());
+        Heal(maxHealth);
+        isAlive = true;
+        isDead = false;
+        movement.SetPlayable(true);
     }
 
     public Transform GetLastCheckPoint() {
-        float NearestDistance = 0;
-        float ClosestCheckPoint = 0;
+        float NearnestDistance = 0;
+        float CurrentDistance = 0;
         List<Transform> OrderCheckPoints = new List<Transform>();
-        NearestDistance = Vector3.Distance(transform.position, checkPoints[0].position);
+        NearnestDistance = Vector3.Distance(transform.position, checkPoints[0].position);
         for (int i = 0; i < checkPoints.Count; i++) {
-            ClosestCheckPoint = Vector3.Distance(transform.position, checkPoints[i].position);
-            if (ClosestCheckPoint <= NearestDistance) {
+            CurrentDistance = Vector3.Distance(transform.position, checkPoints[i].position);
+            if (CurrentDistance <= NearnestDistance) {
                 OrderCheckPoints.Add(checkPoints[i]);
-                NearestDistance = ClosestCheckPoint;
+                NearnestDistance = CurrentDistance;
             }
         }
         int finalCheckPoint =  OrderCheckPoints.Count - 1;
         return OrderCheckPoints[finalCheckPoint];
     }
 
-    private void SetDefaultState() {
+    public override void SetDefaultState() {
         try {
             inventary = GetComponent<PlayerInventary>();
             movement = GetComponent<PlayerMovement>();
-            //TeleportTo(InitialPlayerPosition);
         } catch {
             Debug.LogError("Error setting default state");
         }
         
     }
+
 }
