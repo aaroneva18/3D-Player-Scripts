30578578fe894d90433a3856d5fbe227568c9f43
aaroneva <aaronalvaradocalderon@gmail.com>
2025-01-29 17:59:41 -0600

    improving PlayerInteractable, updating PutObjectOnHand function and improving StoreItem

--- Player Manager/PlayerInteractable.cs
+++ Player Manager/PlayerInteractable.cs
@@ -16,9 +16,11 @@ public class PlayerInteractable : MonoBehaviour
 
     [SerializeField] private float RayLength = 0.0f;
     [SerializeField] private bool IsRightHandEmpty = true;
+    [SerializeField] private bool IsLeftHandEmpty = true;
     [SerializeField] private LayerMask interactableLayer;
     [SerializeField] private Transform CameraTransform;
     [SerializeField] private Transform RightHand;
+    [SerializeField] private Transform LeftHand;
 
     private void Awake() {
         SetDefaultState();
@@ -28,6 +30,9 @@ public class PlayerInteractable : MonoBehaviour
     {
         InteractWithItem();
     }
+
+    public bool GetIsRightHandEmpty { get { return IsRightHandEmpty; } }
+    public bool GetIsLeftHandEmpty { get { return IsLeftHandEmpty; } }
 
     private void InteractWithItem() {
         Debug.DrawRay(CameraTransform.position, CameraTransform.forward * RayLength, Color.red);
@@ -44,10 +49,11 @@ public class PlayerInteractable : MonoBehaviour
         bool hasSpace = inventory.GetSize < inventory.GetMaxSize;
 
         if (canCollect && hasSpace) {
-            inventory.AddItemToInventory(item.gameObject.name, item.gameObject);
+            inventory.AddItemToInventory(item.name, item.gameObject);
+            Debug.Log("Item collected: " + item.name);
 
             if (IsRightHandEmpty) {
-                PutObjectOnRightHand(item.gameObject);
+                PutObjectOnHand(LeftHand, item.gameObject, false);
             } else {
                 item.gameObject.SetActive(false);
             }
@@ -60,20 +66,32 @@ public class PlayerInteractable : MonoBehaviour
 
     }
 
-    private void PutObjectOnRightHand(GameObject p_object) {
-        if (p_object == null || RightHand == null) { return; }
+    private void PutObjectOnHand(Transform p_hand, GameObject p_object, bool IsRightHand) {
+        if (p_object == null || p_hand == null) { return; }
 
-        IsRightHandEmpty = false;
-        Rigidbody rb = p_object.GetComponent<Rigidbody>();
-        if (rb != null) {
-            rb.isKinematic = true;
-            rb.useGravity = false;
+        if ((IsRightHand && !IsRightHandEmpty) || (!IsRightHand && !IsLeftHandEmpty)) {
+            Debug.Log("Hand is not empty");
+            return;
         }
 
-        //agregar la rotacion de la camara
-        p_object.transform.SetParent(RightHand); 
+        Rigidbody rb = p_object.GetComponent<Rigidbody>();
+        Collider c = p_object.GetComponent<Collider>();
+        if (rb != null && c != null) {
+            rb.isKinematic = true;
+            rb.useGravity = false;
+            c.enabled = false;
+        }
+
+        p_object.transform.SetParent(p_hand); 
         p_object.transform.localPosition = Vector3.zero; 
         p_object.transform.localRotation = Quaternion.identity;
+
+        if (IsRightHand) {
+            IsRightHandEmpty = false;
+        } else {
+            IsLeftHandEmpty = false;
+        }
+
     }
 
     private void SetDefaultState() {
