e08132032b152d26453de0cd97e10f43ba3ccf30
aaroneva <aaronalvaradocalderon@gmail.com>
2025-02-10 23:52:55 -0600

    first draft of changeDetectionRadius

--- Player Manager/PlayerDetectable.cs
+++ Player Manager/PlayerDetectable.cs
@@ -7,7 +7,6 @@ public class PlayerDetectable : MonoBehaviour
     [SerializeField] float detectionRadius = 0;
     [SerializeField] float MaxDetectionRadius = 0;
 
-
     private PlayerMovement playerMovement;
 
     private void Awake() {
@@ -21,16 +20,31 @@ public class PlayerDetectable : MonoBehaviour
 
     void Update()
     {
-        
+        ChangeDetectionRadius();
     }
 
     public float GetDetectionRadius { get { return detectionRadius; } }
 
     public void ChangeDetectionRadius() {
-
+        if (playerMovement.GetIsMoving) {
+            IncreaseDetectionRadious(1);
+        }
     }
-    public void IncreaseDetectionRadious(float increasePoints) {}
-    public void DecreaseDetectionRadious(float decreasePoints) {}
+    public void IncreaseDetectionRadious(float increasePoints) {
+        if (detectionRadius < MaxDetectionRadius) {
+            detectionRadius += increasePoints;
+        } else {
+            detectionRadius = MaxDetectionRadius;
+        }
+    }
+
+    public void DecreaseDetectionRadious(float decreasePoints) {
+        if (detectionRadius > 0) {
+            detectionRadius -= decreasePoints;
+        } else {
+            detectionRadius = 0;
+        }
+    }
 
     private void SetDefaultState() {
         try {
